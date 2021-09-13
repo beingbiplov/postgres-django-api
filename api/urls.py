826c91f02e0c114b9049997fb51c68402b1b2ade
beingbiplov <beingbiplov@gmail.com>
2021-09-13 11:18:59 +0545

    update url paths

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import index, StudentListView,StudentDetailView
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('student-list', StudentListView.as_view(), name='student-list'),
+    path('', StudentListView.as_view(), name='student-list'),
     path('student-detail/<int:pk>', StudentDetailView.as_view(), name='student-detail'),
+    path('counts', index, name='counts'),
 ]
