88ee97f859ca42ab6ac13b69fe1bf12f1fd3ab54
beingbiplov <beingbiplov@gmail.com>
2021-10-10 18:28:21 +0545

    add test for models

--- api/tests/test_models.py
+++ api/tests/test_models.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from api.models import Student
 
-# Create your tests here.
+class StudentModelTest(TestCase):
+	@classmethod
+	def setUpTestData(cls):
+		Student.objects.create(first_name='Adam', last_name='Smith', gender='Male', roll_no=1)
+
+	def test_first_name_label(self):
+		student = Student.objects.get(id=1)
+		field_label = student._meta.get_field('first_name').verbose_name
+		self.assertEqual(field_label, 'first name')
+
+	def test_first_name_maxlength(self):
+		student = Student.objects.get(id=1)
+		max_length = student._meta.get_field('first_name').max_length
+		self.assertEqual(max_length, 50)
+
+	def test_object_name(self):
+		student = Student.objects.get(id=1)
+		expected_obj_name = f'{student.first_name} {student.last_name}'
+		self.assertEqual(expected_obj_name, 'Adam Smith')
