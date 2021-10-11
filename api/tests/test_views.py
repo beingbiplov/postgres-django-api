36ab0e4b5e5181b92eb3167ba37b8006a3998dc4
beingbiplov <beingbiplov@gmail.com>
2021-10-11 09:28:06 +0545

    add test for views

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from api.views import index
+from django.urls import reverse
+from api.models import Student
 
-# Create your tests here.
+class IndexViewTest(TestCase):
+
+	def test_url_exists(self):
+		response = self.client.get('/api/')
+		self.assertEqual(response.status_code, 200)
+
+
+	def test_view_url_accessibilty_by_name(self):
+		response = self.client.get(reverse('student-list'))
+		self.assertEqual(response.status_code, 200)
